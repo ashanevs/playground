c834f5db194e6c8b2f50563e0cc43bc7860b8186
Shane Vann-Shirley <ashanev@gmail.com>
2022-11-06 07:44:38 -0500

    Add link to info on automatic scientific notation conversion

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // after reading https://computer.howstuffworks.com/bytes.htm
+// ... this breaks with something like 1010111101010110101110, 
+// see: https://stackoverflow.com/questions/1685680/how-to-avoid-scientific-notation-for-large-numbers-in-javascript
 function convertBinaryToDecimal(num) {
     const str = String(num);
 
